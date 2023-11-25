5773502922e99a73ca8ae559ed61248c0b06d72a
Yogendra Singh Rawat <105751918+Yogi0804@users.noreply.github.com>
2023-11-25 13:37:35 +0530

    fix: added comment.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ const Navbar = () => {
         to="/"
         className="w-10 h-10 rounded-lg bg-white flex items-center justify-center font-bold shadow-md"
       >
+        {/* Initials */}
         <p className="blue-gradient_text">YR</p>
       </NavLink>
       <nav className="flex text-lg gap-7 font-medium">
